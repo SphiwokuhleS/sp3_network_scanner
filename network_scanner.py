9559bb2736a48492b1269a5bfb047167431253b0
SphiwokuhleS <sphiwokuhleshandu24@gmail.com>
2019-08-29 16:57:22 +0200

    i dont know why i'm commiting

--- network_scanner.py
+++ network_scanner.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 #realtimetranslation.net
+
 import scapy.all as scapy
+#from scapy import all as scapy
 import optparse
 import json
 import urllib.request as urllib
 import codecs
 
+
 def get_arguments():
 
 	parser = optparse.OptionParser()
-	parser.add_option("-t", "--target", dest="target", help="IP ADRESS OF TARGET NETWORK.")
+	parser.add_option("-t", "--target", dest="target", help="IP ADRESS OF TARGET NETWORK.") #using optparse to pass target ip address
+	#parser.add_option("-h", "--help", help="")
 	(options, arguments) =  parser.parse_args()
 
 	return options
@@ -41,20 +45,18 @@ def get_mac_vendor(mac_v):
 	response = urllib.urlopen(req)
 	reader = codecs.getreader("utf-8")
 	obj = json.load(reader(response))
-	reqprint = (obj['result']['company']);
+	reqprint = (obj['result']['company'])
 
 	return reqprint
 
 
 def print_result(result_list):
-
 	print("----------------------------------------------------------------------------------------------")
 	print("IP ADDRESS\t\t\tMAC ADDRESS\t\t\tMAC VENDOR")
 	print("----------------------------------------------------------------------------------------------")
 
 	for client in result_list:
-		print (client["ip"] + "\t\t\t" + client["mac"] + "\t\t" + get_mac_vendor(client["mac"]))
-
+		print (client["ip"] + "\t\t\t" + client["mac"] + "\t\t" + get_mac_vendor(client["mac"])) 
 
 	#print(unanswerd.summary()) #printing the summary of a packet
 	#scapy.ls(scapy.Ether) #checking for fields
